8172e64cf23b369899326d3ea66ba9b2ab3a23b1
bindu-19-code <bindukreddy1111@gmail.com>
2025-10-11 15:48:16 +0530

    Fixed navbar translation and i18nnnn

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ const port = process.env.PORT || 5000;
 // ===== Middleware =====
 app.use(
   cors({
-    origin: "https://<your-netlify-site>.netlify.app",
+    origin: "https://nullclass-internship.netlify.app/",
     methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     credentials: true,
   })

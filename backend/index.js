6b28455dbb2e05d2b72d415349cf153ecc7c3f29
bindu-19-code <bindukreddy1111@gmail.com>
2025-10-11 16:09:38 +0530

    Fixed navbar translation and i18nnnnnnt

--- backend/index.js
+++ backend/index.js
@@ -282,7 +282,6 @@ app.use("/api", subscriptionRoutes);
 app.use("/api/auth", authRoutes);
 
 // ===== MongoDB =====
-const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
